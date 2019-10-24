f14c778f7c724b1ccf6547a57da39c41f1aa1731
“wafflemakr” <wafflemakr@gmail.com>
2019-10-24 09:57:49 -0500

    remove uneccessary networks and mnemonics

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,3 @@
-const HDWalletProvider = require("truffle-hdwallet-provider");
-require("dotenv").config();
-
-const providerFactory = network =>
-  new HDWalletProvider(
-    process.env.MNEMONICS || "", // Mnemonics of the deployer
-    `https://${network}.infura.io/v3/${process.env.INFURA_KEY}`, // Provider URL => web3.HttpProvider
-    0,
-    20
-  );
-
 module.exports = {
   compilers: {
     solc: {
@@ -33,18 +22,6 @@ module.exports = {
       port: 8544,
       network_id: "999",
       gas: 6000000
-    },
-    mainnet: {
-      provider: providerFactory("mainnet"),
-      network_id: 1,
-      gas: 7000000,
-      gasPrice: 10000000000 // 10 Gwei
-    },
-    rinkeby: {
-      provider: providerFactory("rinkeby"),
-      network_id: 4,
-      gas: 6900000,
-      gasPrice: 10000000000 // 10 Gwei
     }
   },
   mocha: {
