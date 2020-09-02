9fa01b6870b26f6cae4f044958cfbac9e4d4f4df
EmelyanenkoK <emelyanenko.kirill@gmail.com>
2020-09-02 16:57:36 +0300

    Fix token stubs

--- migrations/2_deploy_contrats.js
+++ migrations/2_deploy_contrats.js
@@ -1,5 +1,6 @@
 const WXRP = artifacts.require("WXRP");
 const WBTC = artifacts.require("WBTC");
+const WETH = artifacts.require("WETH");
 const Exchange = artifacts.require("Exchange");
 const OrionProxy = artifacts.require("OrionProxy");
 const SafeMath = artifacts.require("SafeMath");
@@ -10,6 +11,7 @@ module.exports = async (deployer, network) => {
   if (network === "development") {
     await deployer.deploy(WXRP);
     await deployer.deploy(WBTC);
+    await deployer.deploy(WETH);
 
     await deployer.deploy(SafeMath);
     await deployer.deploy(LibValidator);
