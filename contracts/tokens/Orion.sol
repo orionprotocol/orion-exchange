f53b3a6807646f619f9e972f5e4e4ffb7124697f
EmelyanenkoK <emelyanenko.kirill@gmail.com>
2020-09-16 12:06:25 +0300

    Fix Orion token stub (wrong decimals)

--- contracts/tokens/Orion.sol
+++ contracts/tokens/Orion.sol
@@ -5,7 +5,7 @@ import '@openzeppelin/contracts/token/ERC20/ERC20Capped.sol';
 
 contract Orion is ERC20Detailed, ERC20Capped{
     constructor()
-        ERC20Detailed("Wrapped Ether", "WETH", 18)
+        ERC20Detailed("Orion", "ORN", 8)
         ERC20Capped(100e6 * 1 ether) public {
     }
 }
