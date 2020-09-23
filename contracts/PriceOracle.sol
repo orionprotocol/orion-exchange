bf9cd968041554e14a142fcd3da72bcb4294bf7f
EmelyanenkoK <emelyanenko.kirill@gmail.com>
2020-09-23 13:50:51 +0300

    Refactor EIP712 staff

--- contracts/PriceOracle.sol
+++ contracts/PriceOracle.sol
@@ -1,12 +1,13 @@
 pragma solidity ^0.6.0;
 pragma experimental ABIEncoderV2;
 
+import "./libs/EIP712Interface.sol";
 /**
  * @title PriceOracle
  * @dev Contract for storing and providing price data for the Orion Protocol
  * @author @EmelyanenkoK
  */
-contract PriceOracle {
+contract PriceOracle is EIP712Interface {
 
     struct Prices {
         address[] assetAddresses;
@@ -19,28 +20,6 @@ contract PriceOracle {
         uint64 price;
         uint64 timestamp;
     }
-    string public constant DOMAIN_NAME = "Orion Exchange";
-    string public constant DOMAIN_VERSION = "1";
-    uint256 public constant CHAIN_ID = 666;
-    bytes32
-        public constant DOMAIN_SALT = 0xf2d857f4a3edcb9b78b4d503bfe733db1e3f6cdc2b7971ee739626c97e86a557;
-
-
-    bytes32 public constant EIP712_DOMAIN_TYPEHASH = keccak256(
-        abi.encodePacked(
-            "EIP712Domain(string name,string version,uint256 chainId,bytes32 salt)"
-        )
-    );
-
-    bytes32 public constant DOMAIN_SEPARATOR = keccak256(
-        abi.encode(
-            EIP712_DOMAIN_TYPEHASH,
-            keccak256(bytes(DOMAIN_NAME)),
-            keccak256(bytes(DOMAIN_VERSION)),
-            CHAIN_ID,
-            DOMAIN_SALT
-        )
-    );
 
     bytes32 public constant PRICES_TYPEHASH = keccak256(
         abi.encodePacked(
