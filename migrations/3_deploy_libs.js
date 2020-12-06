6939ef0e619564865ff4cde4f8e6597480ac760c
EmelyanenkoK <emelyanenko.kirill@gmail.com>
2020-12-06 16:22:21 +0000

    Split libs and contracts migrations

--- migrations/3_deploy_libs.js
+++ migrations/3_deploy_libs.js
@@ -15,20 +15,9 @@ let oraclePubkey = "";
 
 module.exports = async (deployer, network, accounts) => {
   if (network === "development") {
-    oraclePubkey = accounts[2];
     await deployer.deploy(LibValidator);
     await deployer.deploy(LibUnitConverter);
     await deployer.deploy(MarginalFunctionality);
-
-    await deployer.link(LibValidator, Exchange);
-    await deployer.link(LibUnitConverter, Exchange);
-    await deployer.link(MarginalFunctionality,Exchange);
-
-    let priceOracleInstance = await deployer.deploy(PriceOracle, oraclePubkey, Orion.address);
-    let exchangeInstance = await deployProxy(Exchange, {unsafeAllowCustomTypes: true, unsafeAllowLinkedLibraries: true});
-
-    await exchangeInstance.setBasicParams(Orion.address, PriceOracle.address, accounts[0]);
-
   }
   if (network === "ropsten") {
     oraclePubkey = "0xC19D917a88e07e5040cD2443FB3a026838C3b852";
@@ -55,39 +44,10 @@ module.exports = async (deployer, network, accounts) => {
 
 
  if (network === "mainnet") {
-    let oracleAddress = "0x3b9E04C53B45A9386de378ab9c27dddd4E15725F";
-    let allowedMatcher = "0x15E030E12cD2C949181BFf268cbEF26F524d7929"
-
-    let ORN = "0x0258F474786DdFd37ABCE6df6BBb1Dd5dfC4434a"
-    let ETH = "0x0000000000000000000000000000000000000000"
-    let USDT = "0xdac17f958d2ee523a2206206994597c13d831ec7"
-
-    let stakedOrionWeight = 242; // 242/255 94.9%
-    let orionWeight = 190; // 190/255 74.5%
-    let ethWeight = 190; // 190/255 74.5%
-    let usdtWeight = 180; // 180/255 70.6%
-
-    let liquidatorPremium = 12;
-    let priceOverdue = 3 * 3600; // Time after which we need fresh prices
-    let positionOverdue = 30 * 24 * 3600; //Time after which position may be liquidated
 
     await deployer.deploy(LibValidator);
     await deployer.deploy(LibUnitConverter);
     await deployer.deploy(MarginalFunctionality);
-
-    await deployer.link(LibValidator, Exchange);
-    await deployer.link(LibUnitConverter, Exchange);
-    await deployer.link(MarginalFunctionality,Exchange);
-
-    await deployer.deploy(PriceOracle, oracleAddress, Orion.address);
-    let priceOracleInstance = await PriceOracle.deployed();
-
-    let exchangeInstance = await deployProxy(Exchange, {unsafeAllowCustomTypes: true, unsafeAllowLinkedLibraries: true});
-
-    await priceOracleInstance.changePriceProviderAuthorization([oracleAddress],[]);
-    await exchangeInstance.setBasicParams(Orion.address, priceOracleInstance.address, allowedMatcher);
-    await exchangeInstance.updateMarginalSettings([ORN, ETH, USDT], stakedOrionWeight, liquidatorPremium, priceOverdue, positionOverdue);
-    await exchangeInstance.updateAssetRisks([ORN, ETH, USDT], [orionWeight, ethWeight, usdtWeight]);
 
   }
 };
