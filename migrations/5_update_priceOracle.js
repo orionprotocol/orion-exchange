cb26185111b75980b8ec730407db4c3abd416b44
EmelyanenkoK <emelyanenko.kirill@gmail.com>
2020-12-09 18:28:12 +0000

    Fix last migration

--- migrations/5_update_priceOracle.js
+++ migrations/5_update_priceOracle.js
@@ -8,6 +8,8 @@ const MarginalFunctionality = artifacts.require("MarginalFunctionality");
 module.exports = async (deployer, network) => {
   if (network === "mainnet") {
     const oracleAddress = "0x3b9E04C53B45A9386de378ab9c27dddd4E15725F";
+    const ORN = "0x0258F474786DdFd37ABCE6df6BBb1Dd5dfC4434a";
+    await deployer.deploy(PriceOracle, oracleAddress, ORN);
     const priceOracleInstance = await PriceOracle.deployed();
     await priceOracleInstance.changePriceProviderAuthorization([oracleAddress],[]);
   }
