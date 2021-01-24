5f1029512860af6ff3c544b665c6be181285d810
EmelyanenkoK <emelyanenko.kirill@gmail.com>
2021-01-24 19:21:35 +0300

    Remove unused artifacts from migrations

--- migrations/6_upgradeExchange.js
+++ migrations/6_upgradeExchange.js
@@ -1,15 +1,6 @@
 const { deployProxy, upgradeProxy } = require('@openzeppelin/truffle-upgrades');
 
-const WXRP = artifacts.require("WXRP");
-const LINK = artifacts.require("LINK");
-const WBTC = artifacts.require("WBTC");
-const WETH = artifacts.require("WETH");
-const Orion = artifacts.require("Orion");
-const PriceOracle = artifacts.require("PriceOracle");
 const Exchange = artifacts.require("Exchange");
-const SafeMath = artifacts.require("SafeMath");
-const LibValidator = artifacts.require("LibValidator");
-const LibUnitConverter = artifacts.require("LibUnitConverter");
 const MarginalFunctionality = artifacts.require("MarginalFunctionality");
 
 
