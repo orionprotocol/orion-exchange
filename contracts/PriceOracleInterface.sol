b41562d78239f41ee116a6260e1be841e3f01343
koloale <koloale@gmail.com>
2021-04-27 01:04:31 +0300

    Fix Price Oracle

--- contracts/PriceOracleInterface.sol
+++ contracts/PriceOracleInterface.sol
@@ -1,11 +1,9 @@
 pragma solidity 0.7.4;
 pragma experimental ABIEncoderV2;
-contract PriceOracleInterface {
-    struct PriceDataOut {
-        uint64 price;
-        uint64 timestamp;
-    }
-    mapping(address => PriceDataOut) public assetPrices;
-    function givePrices(address[] calldata assetAddresses) external view returns (PriceDataOut[] memory) {
-    }
+
+import "./PriceOracleDataTypes.sol";
+
+interface PriceOracleInterface is PriceOracleDataTypes {
+    function assetPrices(address) external view returns (PriceDataOut memory);
+    function givePrices(address[] calldata assetAddresses) external view returns (PriceDataOut[] memory);
 }
